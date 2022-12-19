9d1d46cb52ee0973f4c883c6a9d3b67c0a095e4d
Jose Maria <josmariaguilar@gmail.com>
2022-12-19 11:22:49 -0400

    fixed vulnerabilities of endpoints and added users with UserDetailsService

--- src/main/java/com/fdryt/nursery/configuration/SecurityConfig.java
+++ src/main/java/com/fdryt/nursery/configuration/SecurityConfig.java
@@ -1,21 +1,54 @@
 package com.fdryt.nursery.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@RequiredArgsConstructor
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .authorizeHttpRequests(
+                        authorize -> {
+                            authorize.requestMatchers("/api/ornamental").permitAll();
+                            authorize.anyRequest().authenticated();
+                        }
+                )
                 .httpBasic(withDefaults())
                 .build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails jose = User.builder()
+                .username("jose")
+                .password(passwordEncoder.encode("maria17"))
+                .roles("USER")
+                .build();
+
+        UserDetails darleen = User.builder()
+                .username("consuelo")
+                .password(passwordEncoder.encode("consuelo17"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(jose, darleen);
     }
 }
