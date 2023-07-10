9fadb56b89ffb37c202f3b04d603ae5308922a99
JoseMaria <josmariaguilar@gmail.com>
2023-07-10 17:12:43 -0400

    add styles to modal signin

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -1,30 +1,62 @@
+import { useId } from 'react'
+
+// TODO: implemneted forget password and remember me
 export const Signin = () => {
+  const id = useId()
+
+  const inputUsername = (
+    <div className='flex flex-col gap-2'>
+      <label
+        className='text-sm font-medium text-white'
+        htmlFor={`${id}-username`}
+      >
+        Nombre de usuario
+      </label>
+      <input
+        className='border text-sm rounded-lg w-full p-3 bg-slate-700 border-slate-600 placeholder-slate-400 text-white'
+        type='text'
+        id={`${id}-username`}
+        placeholder='consuelo'
+        required
+      />
+    </div>
+  )
+
+  const inputPassword = (
+    <div className='flex flex-col gap-2'>
+      <label
+        className='text-sm font-medium text-white'
+        htmlFor={`${id}-password`}
+      >
+        Contrase&ntilde;a
+      </label>
+      <input
+        className='border text-sm rounded-lg w-full p-3 bg-slate-700 border-slate-600 placeholder-slate-400 text-white'
+        type='password'
+        id={`${id}-password`}
+        placeholder='•••••••••••'
+        required
+      />
+    </div>
+  )
+
   return (
-    
-      <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
-        <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
-          <div className='p-6 md:space-y-6 sm:p-8'>
-            <h1 className='text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
-              Inicio de Sesi&oacute;n
-            </h1>
-            <form className='space-y-4 md:space-y-6' action='#'>
-              <div>
-                <label htmlFor='email' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Nombre de usuario</label>
-                <input type='email' name='email' id='email' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='name@company.com' required />
-              </div>
-              <div>
-                <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Contrase&ntilde;a</label>
-                <input type='password' name='password' id='password' placeholder='••••••••' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' required />
-              </div>
-              <div className='flex items-center justify-between'>
-                <a href='#' className='text-sm font-medium text-primary-600 hover:underline dark:text-primary-500'>Forgot password?</a>
-              </div>
-              <button type='submit' className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>
-                Iniciar Sesi&oacute;n
-              </button>
-            </form>
-          </div>
-        </div>
+    <section className='flex justify-center items-center h-screen'>
+      <div className='flex flex-col gap-5 px-10 py-8 w-full max-w-md rounded-lg border bg-slate-800 border-slate-700 '>
+        <h1 className='text-center font-bold tracking-tight text-2xl text-white'>
+          Inicio de Sesi&oacute;n
+        </h1>
+        <form className='flex flex-col gap-7'>
+          {inputUsername}
+          {inputPassword}
+          <button
+            className='w-full text-white mt-2 font-medium rounded-lg text-sm p-3.5 text-center bg-[#275121] hover:bg-[#3b7631]'
+            type='submit'
+          >
+            Iniciar Sesi&oacute;n
+          </button>
+        </form>
       </div>
+    </section>
   )
 }
