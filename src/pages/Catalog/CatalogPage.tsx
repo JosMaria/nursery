10e0fe9d2b88366b8b038af3f450187408404553
JoseMaria <josmariaguilar@gmail.com>
2023-07-13 13:25:06 -0400

    created card to catalog

--- src/pages/Catalog/CatalogPage.tsx
+++ src/pages/Catalog/CatalogPage.tsx
@@ -1,18 +1,21 @@
+import { Card } from './components'
 import { data as PRODUCTS } from './data/store'
 
 export const CatalogPage = () => {
-
   console.log(PRODUCTS)
 
   return (
-    <article className='flex flex-wrap justify-center gap-10'>
+    <article className='flex flex-wrap justify-center gap-12 p-5'>
       {PRODUCTS.content.map(product => (
-        <div key={product.id} className='flex flex-col w-80 bg-slate-100 text-sm px-5 py-3 leading-5'>
-          <p className='font-bold text-lg text-center first-letter:uppercase'>{product.commonName}</p>
-          <p className='font-thin italic'>{product.scientificName} {product.scientistSurnameInitial}</p>
-          <p className='font-light'>{product.family}</p>
-          <p className='bg-slate-300 w-fit px-2 font-medium rounded-lg self-end'>{product.status}</p>
-        </div>
+        <Card
+          key={product.id}
+          id={product.id}
+          commonName={product.commonName}
+          scientificName={product.scientificName}
+          scientistSurnameInitial={product.scientistSurnameInitial}
+          family={product.family}
+          status={product.status}
+        />
       ))}
     </article>
   )
--- src/pages/SharedLayout.tsx
+++ src/pages/SharedLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router';
 import { Header, Footer } from '../components';
 
 export const Home = () => (
-  <div className='flex flex-col justify-between items-center h-screen bg-paint-brownLight'>
+  <div className='flex flex-col justify-between items-center bg-paint-brownLight'>
     <Header />
     <main>
       <Outlet />
