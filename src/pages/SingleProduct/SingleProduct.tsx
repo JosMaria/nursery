b2c9b3866cb2af590445d10a6802b1259255062d
JoseMaria <josmariaguilar@gmail.com>
2023-07-14 15:54:01 -0400

    section description implemented

--- src/pages/SingleProduct/SingleProduct.tsx
+++ src/pages/SingleProduct/SingleProduct.tsx
@@ -1,8 +1,8 @@
 export const SingleProduct = () => {
 
   const contentInfoBase = (
-    <div className='flex flex-col items-center gap-5 bg-paint-brownLight p-2'>
-      <h1 className='text-center text-2xl font-medium first-letter:uppercase'>flor de navidad</h1>
+    <div className='flex flex-col items-center gap-5 bg-paint-brownLight'>
+      <h1 className='font-serif text-center text-2xl font-medium first-letter:uppercase'>flor de navidad</h1>
       <div className='flex flex-col gap-1 w-96'>
         <InfoParagraph word='NOMBRE CIENTIFICO'>
           <p className='first-letter:uppercase'>euphorbia pulcherrima</p>
@@ -23,14 +23,22 @@ export const SingleProduct = () => {
     </div>
   )
 
+  const contentDescription = (
+    <div className='h-1/2 bg-red-100 p-5'>
+      <h2 className="font-normal text-lg font-mono">Descripcion</h2>
+      <p className="text-sm">
+        Planta perennes, herbáceas o leñosas, erectas, rastreras o trepadoras, de hojas muy decorativas. Las hojas son de consistencia y grosor notables, ovales, en forma de corazón o punta de flecha, bastante grande, a veces divididas en lóbulos o incluso en forma de mano.
+      </p>
+    </div>
+  )
+
   const sectionInfo = (
-    <section className='w-1/2 bg-red-300 flex flex-col gap-5'>
+    <section className='w-1/2 bg-red-300 flex flex-col gap-5 p-2'>
       {contentInfoBase}
-      <div className='h-1/2 bg-red-100'>
-        Details
-      </div>
+      {contentDescription}
     </section>
   )
+
   return (
     <>
       <article className='flex gap-10 h-96 bg-red-600 p-5'>
@@ -74,7 +82,7 @@ interface InfoParagraphProps {
 }
 
 const InfoParagraph = ({ word, children }: InfoParagraphProps) => (
-  <div className='grid grid-cols-2 justify-start gap-5 text-sm border-b-2 border-black'>
+  <div className='grid grid-cols-2 justify-start gap-5 text-sm border-b-2 border-b-gray-500'>
     <p className='font-light'>{word}</p>
     {children}
   </div>
