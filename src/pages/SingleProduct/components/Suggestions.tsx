4491b128914badf1e058701b23dd7e904b5f81b1
JoseMaria <josmariaguilar@gmail.com>
2023-07-14 19:04:35 -0400

    add styles to the section suggest

--- src/pages/SingleProduct/components/Suggestions.tsx
+++ src/pages/SingleProduct/components/Suggestions.tsx
@@ -1,10 +1,10 @@
 export const Suggestions = () => (
-  <article className=' bg-green-700 px-10 py-5 flex flex-col gap-5'>
-    <h1>Sugerencias</h1>
-    <section className='bg-green-300 flex gap-10'>
-      <div>Sugerencias 1</div>
-      <div>Sugerencias 2</div>
-      <div>Sugerencias 3</div>
+  <article className=' bg-green-500 px-10 py-5 flex flex-col gap-5'>
+    <h1 className='text-xl font-medium'>Sugerencias segun clasificacion</h1>
+    <section className='bg-green-300 flex flex-wrap gap-10'>
+      <div className="bg-green-700 h-40 w-80 flex items-center justify-center">Sugerencias 1</div>
+      <div className="bg-green-700 h-40 w-80 flex items-center justify-center">Sugerencias 2</div>
+      <div className="bg-green-700 h-40 w-80 flex items-center justify-center">Sugerencias 3</div>
     </section>
   </article>
 )
