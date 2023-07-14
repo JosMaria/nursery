61989e12eae8d31a19861ebe1692cd3c8d524efd
JoseMaria <josmariaguilar@gmail.com>
2023-07-14 00:16:28 -0400

    created base structure to single product page

--- src/pages/Catalog/components/Card.tsx
+++ src/pages/Catalog/components/Card.tsx
@@ -1,5 +1,7 @@
+import { Link } from 'react-router-dom'
 import EmptyImage from '../../../assets/no-image.png'
-import { StatusType } from '../types'
+import { StatusType } from '../../../types'
+import { PublicRoutes } from '../../../routes/routes'
 
 interface Props {
   id: number
@@ -13,11 +15,15 @@ interface Props {
 export const Card = ({ id, commonName, scientificName, scientistSurnameInitial, family, status }: Props) => (
   <div className='flex flex-col w-80 bg-gray-100 p-2 rounded-xl shadow-black shadow-sm hover:shadow-black hover:shadow-md border-none'>
     <img src={EmptyImage} alt={commonName} />
-    <div key={id} className='flex flex-col text-sm leading-5 p-2 cursor-pointer'>
+    <Link
+      key={id}
+      className='flex flex-col text-sm leading-5 p-2 cursor-pointer'
+      to={PublicRoutes.SINGLE_PRODUCT}
+    >
       <p className='font-bold text-lg text-center first-letter:uppercase'>{commonName}</p>
       <p className='font-thin italic'>{scientificName} {scientistSurnameInitial}</p>
       <p className='font-light'>{family}</p>
       <p className='bg-slate-300 rounded-lg w-fit px-3 py-1 font-medium text-xs self-end'>{status}</p>
-    </div>
+    </Link>
   </div>
 )
--- src/pages/SingleProduct/SingleProduct.tsx
+++ src/pages/SingleProduct/SingleProduct.tsx
@@ -1,5 +1,44 @@
 export const SingleProduct = () => {
   return (
-    <h1>SingleProduct</h1>
+    <>
+      <article className='flex gap-10 h-96 bg-red-600 p-5'>
+        <section className='w-1/2 bg-red-400'>
+          Imagenes
+        </section>
+        <section className='w-1/2 bg-red-300 flex flex-col gap-5'>
+          <div className="h-1/2 bg-red-900">
+            Information
+          </div>
+          <div className="h-1/2 bg-red-100">
+            Details
+          </div>
+        </section>
+      </article>
+
+      <article className="bg-blue-700 px-10 py-5 flex flex-col gap-5">
+        <nav className="flex gap-5 bg-blue-400">
+          <div>Description</div>
+          <div>Notas</div>
+        </nav>
+        <hr />
+        <section className="bg-blue-200">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laborum vero itaque. Unde quibusdam architecto exercitationem rem quae omnis quos eligendi, tenetur eos rerum accusamus culpa tempora explicabo, dicta maiores.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laborum vero itaque. Unde quibusdam architecto exercitationem rem quae omnis quos eligendi, tenetur eos rerum accusamus culpa tempora explicabo, dicta maiores.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laborum vero itaque. Unde quibusdam architecto exercitationem rem quae omnis quos eligendi, tenetur eos rerum accusamus culpa tempora explicabo, dicta maiores.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laborum vero itaque. Unde quibusdam architecto exercitationem rem quae omnis quos eligendi, tenetur eos rerum accusamus culpa tempora explicabo, dicta maiores.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laborum vero itaque. Unde quibusdam architecto exercitationem rem quae omnis quos eligendi, tenetur eos rerum accusamus culpa tempora explicabo, dicta maiores.
+        </section>
+      </article>
+
+      <article className=" bg-green-700 px-10 py-5 flex flex-col gap-5">
+        <h1>Sugerencias</h1>
+        <section className="bg-green-300 flex gap-10">
+          <div>Sugerencias 1</div>
+          <div>Sugerencias 2</div>
+          <div>Sugerencias 3</div>
+        </section>
+      </article>
+    </>
+
   )
 }
