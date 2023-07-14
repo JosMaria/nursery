a551771e3fc434af685f3fee604d5e3e0a8450ac
JoseMaria <josmariaguilar@gmail.com>
2023-07-14 18:52:13 -0400

    doing the section technicalsheet

--- src/pages/SingleProduct/components/TechnicalSheet.tsx
+++ src/pages/SingleProduct/components/TechnicalSheet.tsx
@@ -1,22 +1,73 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
+
+const technicalSheetInfo = [
+  {
+    word: 'Origen',
+    info: 'Zonas tropicales y subtropicales de América.'
+  },
+  {
+    word: 'Tamaño',
+    info: 'Hasta 0.7 m de altura.'
+  },
+  {
+    word: 'Floración',
+    info: 'Las flores insignificantes; el espádice, que a menudo se confunde con la flor, puede ser amarillo, rojo, purpúreo, verde manzana, rosa intenso, casi anaranjado, blancas y negras.'
+  },
+  {
+    word: 'Ubicación',
+    info: 'Planta de sombra o media sombra, no resiste los rayos directos del sol, ni los descensos bruscos de temperatura.'
+  },
+  {
+    word: 'Suelo',
+    info: 'Bien aireados y drenados, con un pH óptimo en 5.7. Riego: En Verano unas 3 veces a la semana, en invierno 1 vez por semana.'
+  },
+  {
+    word: 'Fertilización',
+    info: 'Abonar una vez cada 15 días con un fertilizante líquido para plantas de flor, durante primavera y verano.'
+  },
+  {
+    word: 'Poda',
+    info: 'Es de crecimiento compacto, por lo que las podas únicamente se realizan cuando se cosechan las flores. Plagas y enfermedades: Araña roja, Trips, Mosca blanca, orugas, pulgones, nematodos, cochinillas, caracoles. Podredumbre radicular. Antracnosis y Septoria.'
+  },
+  {
+    word: 'Enfermedades',
+    info: 'bacterianas y virosis'
+  },
+  {
+    word: 'Tratamientos Fitosanitarios',
+    info: ''
+  },
+  {
+    word: 'Propagación',
+    info: 'Por semillas, vegetativa y por tejidos vegetales.'
+  },
+  {
+    word: 'Otros',
+    info: 'se comercializa por macetas'
+  }
+]
 
 export const TechnicalSheet = () => (
   <article className='bg-blue-700 px-10 py-5 flex flex-col gap-5'>
-    <nav className='p-2 flex gap-10 text-xl  border-gray-400 border-b-2 font-medium'>
+    <nav className='p-2 flex gap-10 text-xl font-medium'>
       <NavLink to='/product/#/technical-sheet'>
         Ficha Tecnica
       </NavLink>
-      <NavLink to='/product/#/notes'>
+      <NavLink to='/product/#/details'>
+        Detalles
+      </NavLink>
+      <NavLink to='/product/#/notes' >
         Notas
       </NavLink>
     </nav>
 
-    <section className='bg-blue-200 px-5'>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laborum vero itaque. Unde quibusdam architecto exercitationem rem quae omnis quos eligendi, tenetur eos rerum accusamus culpa tempora explicabo, dicta maiores.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laborum vero itaque. Unde quibusdam architecto exercitationem rem quae omnis quos eligendi, tenetur eos rerum accusamus culpa tempora explicabo, dicta maiores.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laborum vero itaque. Unde quibusdam architecto exercitationem rem quae omnis quos eligendi, tenetur eos rerum accusamus culpa tempora explicabo, dicta maiores.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laborum vero itaque. Unde quibusdam architecto exercitationem rem quae omnis quos eligendi, tenetur eos rerum accusamus culpa tempora explicabo, dicta maiores.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laborum vero itaque. Unde quibusdam architecto exercitationem rem quae omnis quos eligendi, tenetur eos rerum accusamus culpa tempora explicabo, dicta maiores.
+    <section className='flex flex-col gap-2 bg-blue-200 p-5 w-2/3'>
+      {technicalSheetInfo.map((item, index) => (
+        <div key={index} className='flex text-sm border-b-2 border-gray-400'>
+          <p className='font-medium min-w-[12em] max-w-[12em]'>{item.word}</p>
+          <p>{item.info}</p>
+        </div>
+      ))}
     </section>
   </article>
 )
