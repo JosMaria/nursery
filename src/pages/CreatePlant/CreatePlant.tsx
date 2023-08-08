3264c2b4fff2bdf673814f08108630039c249598
JoseMaria <josmariaguilar@gmail.com>
2023-08-08 14:24:32 -0400

    create select form using react-select

--- src/pages/CreatePlant/CreatePlant.tsx
+++ src/pages/CreatePlant/CreatePlant.tsx
@@ -1,5 +1,14 @@
-export const CreatePlant = () => {
+import Select from 'react-select'
 
+const families = [
+  { value: 'asparagaceae', label: 'asparagaceae' },
+  { value: 'cactaceae', label: 'cactaceae' },
+  { value: 'marantaceae', label: 'marantaceae' },
+  { value: 'sapindaceae', label: 'sapindaceae' },
+  { value: 'araliaceae', label: 'araliaceae' }
+]
+
+export const CreatePlant = () => {
   const commonNameInput = (
     <div className='flex flex-col gap-1 w-fit'>
       <label
@@ -9,7 +18,7 @@ export const CreatePlant = () => {
         Nombre Com&uacute;n
       </label>
       <input
-        className='p-2 text-sm border rounded-md focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring bg-gray-200 border-gray-200 focus:border-blue-400'
+        className='p-2 text-sm border rounded-md border-gray-200 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:border-blue-400'
         id='commonName'
         type='text'
       />
@@ -26,12 +35,12 @@ export const CreatePlant = () => {
       </label>
       <div className='flex gap-3'>
         <input
-          className='p-2 text-sm border rounded-md focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring bg-gray-200 border-gray-200 focus:border-blue-400'
+          className='p-2 text-sm border rounded-md border-gray-200 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:border-blue-400'
           id='scientificName'
           type='text'
         />
         <input
-          className='w-10 p-2 text-sm border rounded-md focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring bg-gray-200 border-gray-200 focus:border-blue-400'
+          className='w-10 p-2 text-sm border rounded-md focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring border-gray-200 focus:border-blue-400'
           id='scientificName'
           type='text'
         />
@@ -39,14 +48,32 @@ export const CreatePlant = () => {
     </div>
   )
 
-
+  const familyInput = (
+    <div className='flex flex-col gap-1 w-fit'>
+      <label
+        className='font-medium text-sm'
+        htmlFor='family'
+      >
+        Familia
+      </label>
+      <Select
+        className='w-64 text-sm border rounded-md border-gray-200 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:border-blue-400'
+        id='family'
+        isClearable={true}
+        isSearchable={true}
+        options={families}
+        placeholder='Seleccionar...'
+      />
+    </div>
+  )
 
   return (
     <section className='bg-neutral-300 max-w-screen-xl'>
       <h1 className='text-4xl indent-10'>Crear Planta</h1>
-      <form className='flex flex-wrap gap-10 p-5'>
+      <form className='flex flex-wrap gap-5 p-5'>
         {commonNameInput}
         {scientificNameInput}
+        {familyInput}
       </form>
     </section>
   )
