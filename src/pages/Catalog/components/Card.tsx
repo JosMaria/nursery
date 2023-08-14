d92c63758d773bbd1ea23b876b3e3badb199dc88
JoseMaria <josmariaguilar@gmail.com>
2023-08-13 23:33:38 -0400

    fix the card and add styles

--- src/pages/Catalog/components/Card.tsx
+++ src/pages/Catalog/components/Card.tsx
@@ -1,28 +1,37 @@
-import { Link } from 'react-router-dom'
-import EmptyImage from '../../../assets/no-image.png'
-import { StatusType } from '../../../types'
+import { Link } from 'react-router-dom';
+import EmptyImage from '../../../assets/no-image.png';
+import { StatusType } from '../../../types';
 
 interface Props {
-  id: number
-  commonName: string
-  scientificName: string | null
-  scientistSurnameInitial: string | null
-  family: string | null
-  status: StatusType
+  id: number;
+  commonName: string;
+  scientificName: string | null;
+  scientistSurnameInitial: string | null;
+  family: string | null;
+  status: StatusType;
 }
 
-export const Card = ({ id, commonName, scientificName, scientistSurnameInitial, family, status }: Props) => (
-  <div className='flex flex-col w-80 bg-gray-100 p-2 rounded-xl shadow-black shadow-sm hover:shadow-black hover:shadow-md border-none'>
+export const Card = ({
+  id,
+  commonName,
+  scientificName,
+  scientistSurnameInitial,
+  family,
+  status,
+}: Props) => (
+  <div className='flex flex-col gap-1 w-80 bg-stone-50 border border-stone-300 p-2 rounded-xl shadow-black shadow-sm hover:shadow-black hover:shadow-md'>
     <img src={EmptyImage} alt={commonName} />
-    <Link
-      key={id}
-      className='flex flex-col text-sm leading-5 p-2 cursor-pointer'
-      to={`/products/${id}`}
-    >
-      <p className='font-bold text-lg text-center first-letter:uppercase'>{commonName}</p>
-      <p className='font-thin italic'>{scientificName} {scientistSurnameInitial}</p>
-      <p className='font-light'>{family}</p>
-      <p className='bg-slate-300 rounded-lg w-fit px-3 py-1 font-medium text-xs self-end'>{status}</p>
+    <Link key={id} className='cursor-pointer' to={`/products/${id}`}>
+      <p className='font-bold text-lg first-letter:uppercase text-center'>
+        {commonName}
+      </p>
+      <div className='grid grid-rows-3 text-sm px-2 pb-2 h-14'>
+        <p className='italic'>
+          {scientificName} {scientistSurnameInitial}
+        </p>
+        <p className=''>{family}</p>
+        <p className='available-status place-self-end'>{status}</p>
+      </div>
     </Link>
   </div>
-)
+);
