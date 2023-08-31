da4570958faffb751d69921caab9e5746608c6f8
JoseMaria <josmariaguilar@gmail.com>
2023-08-30 20:30:11 -0400

    empty content in sub pages of plant page

--- src/pages/Plant/pages/NotesPage.tsx
+++ src/pages/Plant/pages/NotesPage.tsx
@@ -4,10 +4,14 @@ export const NotesPage = () => {
   const notes = useNotesPlant();
 
   return (
-    <ul className='list-decimal list-inside'>
-      {notes.map((note, index) => (
-        <li key={index}>{note}</li>
-      ))}
+    <ul className='list-decimal list-inside flex flex-col gap-3'>
+      {notes.length === 0 ? (
+        <p className='text-center font-medium text-base py-3 max-sm:text-xs max-sm:py-2 bg-skin-dark text-skin-light'>
+          Sin Notas
+        </p>
+      ) : (
+        notes.map((note, index) => <li key={index}>{note}</li>)
+      )}
     </ul>
   );
 };
--- src/pages/Plant/pages/TechnicalSheetPage.tsx
+++ src/pages/Plant/pages/TechnicalSheetPage.tsx
@@ -5,15 +5,21 @@ export const TechnicalSheetPage = () => {
 
   return (
     <div className='flex flex-col gap-2'>
-      {technicalSheeet.map((data, index) => (
-        <div
-          key={index}
-          className='flex max-md:flex-col md:gap-5 border-b-2 border-black'
-        >
-          <b>{data.word}</b>
-          <p>{data.info}</p>
-        </div>
-      ))}
+      {technicalSheeet.length === 0 ? (
+        <p className='text-center font-medium text-base py-3 max-sm:text-xs max-sm:py-2 bg-skin-dark text-skin-light'>
+          Sin Ficha Tecnica
+        </p>
+      ) : (
+        technicalSheeet.map((data, index) => (
+          <div
+            key={index}
+            className='flex max-md:flex-col md:gap-5 border-b-2 border-black'
+          >
+            <b>{data.word}</b>
+            <p>{data.info}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 };
