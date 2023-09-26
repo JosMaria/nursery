3897d39f78628acb9f5d48c3b70f5d9a333ec9b8
JoseMaria <josmariaguilar@gmail.com>
2023-09-26 19:26:38 -0400

    pages empty

--- src/pages/Catalog/CatalogPage.tsx
+++ src/pages/Catalog/CatalogPage.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchPaginatedProducts } from './service';
 import SkeletonCatalogPage from './SkeletonCatalogPage';
-import { ProductList } from './components';
+import { EmptyList, ProductList } from './components';
 
 const CatalogPage = () => {
   const { data: page, status } = useQuery({
@@ -11,18 +11,9 @@ const CatalogPage = () => {
 
   if (status === 'loading') return <SkeletonCatalogPage />;
   if (status === 'error')
-    return (
-      <p>
-        Se obtuvo un error al cargar los productos, Una opcion es la conexion a
-        Internet
-      </p>
-    );
+    return <p>Se obtuvo un error al cargar los productos, Una opcion es la conexion a Internet</p>;
 
-  return (
-    <>
-      <ProductList products={page.content} />
-    </>
-  );
+  return <>{page.content.length === 0 ? <EmptyList /> : <ProductList products={page.content} />}</>;
 };
 
 export default CatalogPage;
--- src/pages/Catalog/components/index.ts
+++ src/pages/Catalog/components/index.ts
@@ -1,2 +1,3 @@
 export * from './ProductList';
 export * from './Product';
+export * from './EmptyList';
