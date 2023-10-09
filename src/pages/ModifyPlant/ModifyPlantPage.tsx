dc0e5b8640cb6d97404fa9fcffb9d15021e503fe
JoseMaria <josmariaguilar@gmail.com>
2023-10-09 16:30:31 -0400

    created danger zone in the modify plant

--- src/pages/ModifyPlant/ModifyPlantPage.tsx
+++ src/pages/ModifyPlant/ModifyPlantPage.tsx
@@ -1,6 +1,11 @@
 import { useParams } from 'react-router-dom';
 import { fetchProductByID } from './services';
 import { useQuery } from '@tanstack/react-query';
+import { EditButton, DangerZone } from './components';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
+import { InferType, array, object, string } from 'yup';
 
 const ModifyPlantPage = () => {
   const { id } = useParams();
@@ -16,15 +21,17 @@ const ModifyPlantPage = () => {
 
   return (
     <section>
-      <h1 className='text-2xl font-medium'>{plantToModify.commonName}</h1>
+      {/* <h1 className='text-2xl font-medium'>Configuraci&oacute;n de la planta</h1> */}
+      <DangerZone />
+
+      {/* <SectionInfoBase commonName={plantToModify.}classifications={plantToModify.classifications}/> */}
+      {/*           
       <details>
         <summary className='text-lg font-mono'>INFORMACI&Oacute;N BASICA</summary>
         <div className='flex flex-col gap-5'>
-          <article className='flex gap-5'>
-            <p>Nombre cientifico</p>
-            <p>{plantToModify.scientificName}</p>
-            <button className='border-2 border-black'>Modificar</button>
-          </article>
+          
+          
+
           <article className='flex gap-5'>
             <p>Inicial del nombre cientifico</p>
             <p>{plantToModify.scientistLastnameInitial}</p>
@@ -58,8 +65,65 @@ const ModifyPlantPage = () => {
             <li key={index}>{note}</li>
           ))}
         </ul>
-      </details>
+      </details> */}
     </section>
+  );
+};
+const infoBaseSchema = object({
+  commonName: string()
+    .matches(/^(?!\s*$).+/, 'Nombre común obligatorio')
+    .required('Nombre común requerido'),
+  scientificName: string().default(''),
+  scientistLastnameInitial: string().max(1, 'Max 1 letra').default(''),
+  family: string().default(''),
+  status: string().required(),
+  classifications: array(string().required()).default([]),
+});
+
+type InfoBaseSchemaType = InferType<typeof infoBaseSchema>;
+
+const SectionInfoBase = (infoBaseToModify: InfoBaseSchemaType) => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<InfoBaseSchemaType>({
+    defaultValues: infoBaseToModify,
+  });
+
+  return (
+    <>
+      {/* <form className='bg-skin-form p-3 flex flex-col gap-5'>
+        <DevTool />
+      </form> */}
+    </>
+  );
+};
+
+interface InputFieldProps {
+  textLabel: string;
+  value: string | null;
+}
+
+const FieldInput = ({ textLabel, value }: InputFieldProps) => {
+  const [enableEdit, setEnableEdit] = useState(false);
+
+  return (
+    <>
+      <fieldset className='flex flex-col gap-1'>
+        <label className='font-medium text-sm'>{textLabel}</label>
+        <div className='flex gap-2 justify-between items-center'>
+          <input
+            defaultValue={value ? value : ''}
+            className={`custom-input-form ${enableEdit && 'cursor-not-allowed text-gray-500'}`}
+            readOnly={enableEdit}
+            placeholder={value ? '' : 'sin valor'}
+          />
+          <EditButton isShow={enableEdit} action={() => setEnableEdit((prev) => !prev)} />
+        </div>
+      </fieldset>
+    </>
   );
 };
 
