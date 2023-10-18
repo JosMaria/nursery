86a63878c2445d6e05a45b21d3e7604184374eb6
JoseMaria <josmariaguilar@gmail.com>
2023-10-18 10:16:51 -0400

    add domain

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api/v1/',
+  baseURL: 'http://192.168.60.152:8080/api/v1/',
 });
--- src/pages/ModifyPlant/components/InfoBaseZone.tsx
+++ src/pages/ModifyPlant/components/InfoBaseZone.tsx
@@ -1,3 +1,4 @@
+import { EditButton } from '.';
 import { useInfoBase } from '../hooks';
 
 export const InfoBaseZone = () => {
@@ -5,10 +6,44 @@ export const InfoBaseZone = () => {
     useInfoBase();
 
   return (
-    <section className='bg-skin-form rounded-md p-3'>
-      <p>{commonName}</p>
-      <p>{scientificName}</p>
-      <p>{scientistLastnameInitial}</p>
+    <section className='bg-skin-form rounded-md p-3 flex flex-col gap-2'>
+      <fieldset className='flex flex-col gap-0.5 w-72'>
+        <label className='font-medium text-sm'>Nombre Com&uacute;n</label>
+        <div className='flex gap-2 justify-between items-center'>
+          <p className='bg-skin-light p-1'>{commonName}</p>
+          <input
+            defaultValue={commonName}
+            className={`custom-input-form`}
+            readOnly={false}
+          />
+          <EditButton isShow={true} />
+        </div>
+      </fieldset>
+
+      <fieldset className='flex flex-col gap-0.5 w-72'>
+        <label className='font-medium text-sm'>Nombre Cientifico</label>
+        <div className='flex gap-2 justify-between items-center'>
+          <input
+            defaultValue={scientificName || 'sin nombre cientifico'}
+            className={`custom-input-form`}
+            readOnly={false}
+          />
+          <EditButton isShow={true} />
+        </div>
+      </fieldset>
+      
+      <fieldset className='flex flex-col gap-0.5 w-52'>
+        <label className='font-medium text-sm'>Inicial Nombre Cientifico</label>
+        <div className='flex gap-2 justify-between items-center'>
+          <input
+            defaultValue={scientistLastnameInitial || 'sin inicial'}
+            className={`custom-input-form`}
+            readOnly={false}
+          />
+          <EditButton isShow={true} />
+        </div>
+      </fieldset>
+
       <p>{family}</p>
       <p>{status}</p>
       <p>{classifications}</p>
