15639fa4076e37b4e39a5c30a9faacf67417a5b5
JoseMaria <josmariaguilar@gmail.com>
2023-10-30 18:47:05 -0400

    make responsive the accounts page

--- src/pages/Accounts/AccountsPage.tsx
+++ src/pages/Accounts/AccountsPage.tsx
@@ -42,13 +42,15 @@ const USERS: UserResponseType[] = [
 
 const AccountsPage = () => {
   return (
-    <div className='flex flex-col gap-3 w-full items-center'>
-      <h1 className='text-xl font-normal'>Cuentas de Administradores y Asistentes</h1>
-      <table className='max-w-3xl w-full'>
-        <TableHeader />
-        <TableBody />
-      </table>
-    </div>
+    <section className='flex flex-col gap-3 w-full items-center'>
+      <h1 className='text-xl max-sm:text-lg font-medium text-center'>Cuentas de Administradores y Asistentes</h1>
+      <article className='max-w-2xl w-full overflow-x-scroll'>
+        <table className='w-full'>
+          <TableHeader />
+          <TableBody />
+        </table>
+      </article>
+    </section>
   );
 };
 
@@ -56,7 +58,7 @@ const TableHeader = () => (
   <thead>
     <tr className='bg-skin-nav text-skin-light'>
       {['Nombre', 'Apellido', 'Usuario', 'Role', ''].map((titleHeader, index) => (
-        <th key={index} className='py-2 px-4 font-medium text-sm'>
+        <th key={index} className='py-2 px-4 font-medium text-sm max-sm:text-xs'>
           {titleHeader}
         </th>
       ))}
@@ -67,12 +69,12 @@ const TableHeader = () => (
 const TableBody = () => (
   <tbody className=''>
     {USERS.map((user) => (
-      <tr key={user.id} className='text-center text-sm even:bg-skin-light bg-slate-200'>
+      <tr key={user.id} className='text-center text-sm max-sm:text-xs even:bg-skin-light bg-slate-200 whitespace-nowrap py-10'>
         <td className='py-1.5 px-4'>{user.firstname}</td>
         <td className='py-1.5 px-4'>{user.lastname}</td>
         <td className='py-1.5 px-4'>{user.username}</td>
         <td className='py-1.5 px-4'>{user.role}</td>
-        <td className='py-1.5 flex justify-start'>
+        <td className='py-1.5 px-2 flex justify-start items-center '>
           <Link
             className='bg-teal-700 hover:bg-teal-600 focus:ring-2 focus:ring-teal-500 focus:outline-none rounded-md text-white px-1.5 py-1'
             to='/'
