1407426308a7cfe71244cbabce038b596a9d5ef8
JoseMaria <josmariaguilar@gmail.com>
2023-11-14 16:51:18 -0400

    adding tickets status to plant card

--- src/pages/Catalog/components/PlantCard.tsx
+++ src/pages/Catalog/components/PlantCard.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import EmptyImage from '../../../assets/no-image.png';
 import { StatusType } from '../../../types';
-import { getStyledGivenStatus, traduceStatus } from '../../../utils';
+import { traduceStatus } from '../../../utils';
 
 interface PlantCardProps {
   content: {
@@ -37,32 +37,21 @@ export const PlantCard = ({ content }: PlantCardProps) => (
         <p>{content.family}</p>
       </div>
 
-      <p className='self-end font-medium text-xs bg-green-200 text-green-900 px-2 justify-self-end rounded-xl border-2 border-green-700'>
+      <p className={`${styleGivenStatus(content.status)} self-end`}>
         {traduceStatus(content.status)}
       </p>
     </div>
   </Link>
 );
 
-/*
-<Link
-    to='#'
-    className='bg-gradient-to-r from-card-start to-card-end shadow-black shadow-sm hover:shadow-black hover:shadow-xl p-1 flex flex-col gap-1 w-80 max-xl:w-72 max-md:w-60 max-sm:w-48 max-xs:w-40 text-center rounded-b-lg'
-  >
-    <img src={EmptyImage} alt={content.commonName} />
-    <div className='h-24 max-xs:h-20 flex flex-col justify-evenly max-sm:text-sm px-2 max-sm:px-1'>
-      <p className='font-medium max-sm:font-normal whitespace-nowrap overflow-x-auto overflow-y-hidden first-letter:uppercase'>
-        {content.commonName}
-      </p>
-      <div className='flex flex-col items-start justify-between text-sm max-sm:text-xs max-xs:text-xs leading-none h-fit overflow-x-auto overflow-y-hidden whitespace-nowrap'>
-        <p className='leading-none'>
-          {content.scientificName} {content.scientistLastnameInitial}
-        </p>
-        <p className='leading-none'>{content.family}</p>
-      </div>
-      <p className={`${getStyledGivenStatus(content.status)} self-end w-fit`}>
-        {traduceStatus(content.status)}
-      </p>
-    </div>
-  </Link>
-*/
+const styleGivenStatus = (status: StatusType): string => {
+  const styleGeneral = 'px-2 rounded-lg font-medium text-xs border-2';
+
+  if (status === 'AVAILABLE') {
+    return `${styleGeneral} bg-green-200 text-green-800 border-green-800`;
+  } else if (status === 'NON_EXISTENT') {
+    return `${styleGeneral} bg-purple-200 text-purple-800 border-purple-800`;
+  } else if (status === 'IN_CONSERVATION') {
+    return `${styleGeneral} bg-amber-200 text-amber-800 border-amber-800`;
+  } else return '';
+};
