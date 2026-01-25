169212b2f88d1240fea10b11e6995a0be7087063
Jose Maria <josmariaguilar@gmail.com>
2026-01-24 23:43:07 -0400

    added quarkus-hibernate-orm dependency

--- catalog-service/src/main/java/com/lievasoft/entity/Plant.java
+++ catalog-service/src/main/java/com/lievasoft/entity/Plant.java
@@ -1,17 +1,38 @@
 package com.lievasoft.entity;
 
 import com.lievasoft.dto.PlantCreateDto;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+@Entity
+@Table(name = "plants")
 public class Plant {
 
+    @Id
+    @SequenceGenerator(name = "sequence", sequenceName = "plant_sequence")
+    @GeneratedValue(strategy = SEQUENCE, generator = "sequence")
     private Long id;
+
+    @Column(name = "common_name", nullable = false)
     private String commonName;
+
+    @Column(name = "scientific_name")
     private String scientificName;
+
+    @Column(name = "is_available")
     private boolean isAvailable;
+
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    public Plant() {
+    }
 
     public Plant(PlantCreateDto plantCreateDto) {
         this.commonName = plantCreateDto.commonName();
