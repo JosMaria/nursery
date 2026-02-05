dd2abbd8c655c7ccbcf191e1b4286b8611b36869
Jose Maria <josmariaguilar@gmail.com>
2026-02-05 07:48:38 -0400

    Adding cache

--- catalog-service/src/main/java/com/lievasoft/service/PlantService.java
+++ catalog-service/src/main/java/com/lievasoft/service/PlantService.java
@@ -1,23 +1,29 @@
 package com.lievasoft.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lievasoft.dto.PlantCardResponse;
 import com.lievasoft.dto.PlantCreateDto;
 import com.lievasoft.dto.PlantResponseCreateDto;
 import com.lievasoft.entity.Plant;
 import com.lievasoft.repository.PlantRepository;
+import io.quarkus.redis.datasource.RedisDataSource;
+import io.quarkus.redis.datasource.value.ValueCommands;
 import jakarta.enterprise.context.ApplicationScoped;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @ApplicationScoped
 public class PlantService {
 
-    private static final List<Plant> PLANTS = new ArrayList<>();
+    private final ValueCommands<String, String> valueCommands;
     private final PlantRepository plantRepository;
+    private final ObjectMapper objectMapper;
 
-    public PlantService(PlantRepository plantRepository) {
+    public PlantService(PlantRepository plantRepository, RedisDataSource redisDataSource, ObjectMapper objectMapper) {
         this.plantRepository = plantRepository;
+        valueCommands = redisDataSource.value(String.class);
+        this.objectMapper = objectMapper;
     }
 
     public PlantResponseCreateDto create(PlantCreateDto payload) {
@@ -27,6 +33,22 @@ public class PlantService {
     }
 
     public List<PlantCardResponse> fetchPlantCards() {
-        return plantRepository.fetchPlantCards();
+        var plantCards = plantRepository.fetchPlantCards();
+        cachePlantList(plantCards);
+        return plantCards;
+    }
+
+    public void cachePlantList(List<PlantCardResponse> plantCards) {
+        String plantCardsJson = serializePlantList(plantCards);
+        valueCommands.set("plantCards:list", plantCardsJson);
+    }
+
+    private String serializePlantList(List<PlantCardResponse> plantCards) {
+        try {
+            return objectMapper.writeValueAsString(plantCards);
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error serializing plant list", e);
+        }
     }
 }
